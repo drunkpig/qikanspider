ac2c65dc6a31d2bca5673150b6ef79dd55a525cb
jon <xuchaoo@gmail.com>
2014-10-17 23:21:30 +0800

    抓抓，我抓

--- index.php
+++ index.php
@@ -1,51 +1,23 @@
 <?php 
+require_once "./lib/functions.php";
 
 
-function parse_img_url($content)
+$portal = array(
+                "自然科学与工程技术" => "http://epub.cnki.net/kns/oldnavi/n_Navi.aspx?NaviID=116&Flg=", 
+                "人文社会科学"       => "http://epub.cnki.net/kns/oldnavi/n_Navi.aspx?NaviID=117&Flg="
+			   );
+
+foreach($portal as $k=>$u)
 {
-	
-	$pattern = "/<img width=206 src='\/fengmian\/(.*?)'/";
-	$match = array();
-	preg_match($pattern, $content, $match);
-	$u = $match[1];
-	$arr = explode("/", $u);
-	$u = end($arr);
-	return $u;
+	$content = file_get($u);
+	$urls = parse_entry_page($content);
+	foreach($urls as $c=>$pageu)
+	{
+		$info = get_a_class_of_qikan($pageu);
+		//echo "SAVE $c\n";
+		//TODO 保存
+	}
 }
 
-function get_image_big($img)
-{
-	$bigUrl = "http://c61.cnki.net/CJFD/big/" . $img;
-	$content = file_get_contents($bigUrl);
-	$img_big_dir = "./big/";
-	file_put_contents($img_big_dir . $img, $content);
-}
-
-function get_image_small($img)
-{
-	$img_small_dir = "./small/";
-	$smallUrl = "http://c61.cnki.net/CJFD/small/" . $img;
-	$content = file_get_contents($smallUrl);
-	file_put_contents($img_small_dir . $img, $content);
-	
-}
-
-
-
-
-
-
-?>
-
-<?php 
-$u = "http://epub.cnki.net/kns/oldnavi/n_CNKIPub.aspx?naviid=110&BaseID=JSJJ&NaviLink=%E7%94%B5%E5%AD%90%E4%BF%A1%E6%81%AF%E7%A7%91%E5%AD%A6%E7%BB%BC%E5%90%88-%2fkns%2foldnavi%2fn_list.aspx%3fNaviID%3d100%26Field%3d168%25e4%25b8%2593%25e9%25a2%2598%25e4%25bb%25a3%25e7%25a0%2581%26Value%3dI000%253f%26OrderBy%3didno%7c%E8%AE%A1%E7%AE%97%E6%9C%BA%E9%9B%86%E6%88%90%E5%88%B6%E9%80%A0%E7%B3%BB%E7%BB%9F";
-
-$content = file_get_contents($u);
-
-file_put_contents("./a.html", $content);
-$imageU = parse_img_url($content);
-echo $imageU;
-get_image_big($imageU);
-get_image_small($imageU);
 
 ?>
---
+++ lib/functions.php
@@ -0,0 +1,228 @@
+<?php 
+require_once "simple_html_dom.php";
+require_once "HttpClient.class.php";
+
+function parse_img_url($content)
+{
+	
+	$pattern = "/<img width=206 src='\/fengmian\/(.*?)'/";
+	$match = array();
+	preg_match($pattern, $content, $match);
+	$u = $match[1];
+	$arr = explode("/", $u);
+	$u = end($arr);
+	return $u;
+}
+
+function get_image_big($img)
+{
+	$bigUrl = "http://c61.cnki.net/CJFD/big/" . $img;
+	$content = file_get_contents($bigUrl);
+	$img_big_dir = "./big/";
+	file_put_contents($img_big_dir . $img, $content);
+}
+
+function get_image_small($img)
+{
+	$img_small_dir = "./small/";
+	$smallUrl = "http://c61.cnki.net/CJFD/small/" . $img;
+	$content = file_get_contents($smallUrl);
+	file_put_contents($img_small_dir . $img, $content);
+	
+}
+
+/**
+ * 大类别＃次级类别=>链接
+ * 数组形式
+ */
+function parse_entry_page($content)
+{
+	$baseUrl = "http://epub.cnki.net/kns/oldnavi/";
+	$dom = new simple_html_dom(); 
+	$html = $dom->load($content);
+	$blocks = $html->find("div.col");
+	$array = array();
+	foreach($blocks as $blk)
+	{
+		$title = $blk->find("h5");
+		$title = $title[0]->plaintext;
+		$title = trim($title);
+		$title = substr($title, 0, strpos($title, "("));
+		
+		$subClass = $blk->find("li");
+		foreach($subClass as $c)
+		{
+			$subTitle = $c->plaintext;
+			$subTitle = trim($subTitle);
+			$subTitle = substr($subTitle, 0, strpos($subTitle, "("));
+			// 寻找URL
+			$u = $c->find("a");
+			$u = $u[0];
+			$u = $u->href;
+			$u = $baseUrl . $u;
+			$array["$title#$subTitle"] = $u;
+		}
+	}
+	$dom->clear();
+	return $array;
+}
+
+function get_cache_file_name($url)
+{
+	$fpath = "./cache/" . md5($url) . ".html";
+	return $fpath;
+}
+
+function file_get($url)
+{
+	$fname = get_cache_file_name($url);
+	if(file_exists($fname))
+	{
+		//echo "cache hit!\n";
+		return file_get_contents($fname);
+	}
+	else
+	{
+		//echo "cache miss!\n";
+		$c = file_get_contents($url);
+		file_put_contents($fname, $c);
+		return $c;
+	}
+}
+
+function parse_page_count($content)
+{
+	$dom = new simple_html_dom();
+	$html = $dom->load($content);
+	$page = $html->find("span#lblCount");
+	$page = $page[0]->plaintext;
+	$dom->clear();
+	return $page;
+}
+
+function parse_paged_url($content)
+{
+	$urlBase = "http://epub.cnki.net/kns/oldnavi/";
+	$result = array();
+	$dom = new simple_html_dom();
+	$html = $dom->load($content);
+	$qikans = $html->find("div.colPic");
+	foreach($qikans as $q)
+	{
+		$a = $q->find("p a");
+		$a = $a[0];
+		$name = $a->plaintext;
+		$url = $a->href;
+		$url = $urlBase. $url;
+		$result[$name] = $url;
+	}
+	$dom->clear();
+	return $result;
+}
+
+function get_view_state($content)
+{
+	$dom = new simple_html_dom();
+	$html = $dom->load($content);
+	$viewState = $html->find("input[name=__VIEWSTATE]");
+	$v = $viewState[0];
+	$v =  $v->value;
+	return $v;
+}
+
+/*
+hidUID:
+hidType:CJFQ
+drpField:cykm$%"{0}"
+txtValue:
+DisplayModeRadio:图形方式
+drpAttach:order by idno
+
+DisplayModeRadio11:图形方式
+drpAttach:order by idno
+
+*/
+function parse_post_data($i, $content)
+{
+	$data = array();
+	$data["__EVENTTARGET"] = "lbNextPage";
+	$data["__EVENTARGUMENT"] = "";
+	$data["__VIEWSTATE"] = get_view_state($content);
+	$data["txtPageGoTo"] = $data["txtPageGoToBottom"] = $i;
+	$data["hidUID"] = "";
+	$data["hidType"] = "CJFQ";
+	$data["drpField"] = 'cykm$%"{0}"';
+	$data["txtValue"] = "";
+	$data["DisplayModeRadio"] = "图形方式";
+	$data["drpAttach"] = "order by idno";
+	$data["DisplayModeRadio11"] = "图形方式";
+	$data["drpAttach"] = "order by idno";
+	return $data;
+}
+
+function fsave($name, $content)
+{
+	if(strlen($content)<500)
+	{
+		echo "Empty page\n";
+		return false;
+	}
+	file_put_contents($name, $content);
+	return true;
+}
+
+function parse_ot_page($pageCount, $firstPageContent, $startUrl)
+{
+	$client = new HttpClient('epub.cnki.net');
+	$client->get($startUrl);//目的是为了获取cookie和ref头
+
+	$result = array();
+	$pageContent = $firstPageContent;
+	for($i=1; $i<$pageCount; $i++)//从第二页开始到$pageCount页
+	{
+		$fname = get_cache_file_name($startUrl.$i);
+		if(file_exists($fname))
+		{
+			$pageContent = file_get($fname);
+		}
+		else
+		{
+			$postData = parse_post_data($i, $pageContent);
+			//填充post的参数
+			$pageContent = $client->post($startUrl, $postData);
+			if(!fsave($fname, $pageContent))
+			{
+				echo "Empty Content\n";
+				continue;
+			}
+		}
+
+		//解析页面内容
+		echo "解析。。。。\n";
+		$o = parse_paged_url($pageContent);
+		$result = array_merge($result, $o);
+	}
+	
+	return $result;
+}
+
+/**
+ * 解析一个类别的，多页
+ */
+function get_a_class_of_qikan($startUrl)
+{
+	$content = file_get($startUrl);
+	//1,找出来有多少页
+	$pageCount = parse_page_count($content);
+	//2,获取页面上具体的杂志地址
+	$nameUrl = parse_paged_url($content);
+	//3,循环获取其他分页中的
+	$ot = array();
+	if($pageCount>1)
+	{
+		$ot = parse_ot_page($pageCount, $content, $startUrl);
+	}
+	$nameUrl = array_merge($nameUrl, $ot);
+	return $nameUrl;
+}
+?>
