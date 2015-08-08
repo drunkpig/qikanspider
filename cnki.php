03fbdcf1a343ee0248f7b111a2188d0c83ca7e27
Jon <xuchaoo@gmail.com>
2015-08-08 19:57:17 +0800

    rename file

---
+++ cnki.php
@@ -0,0 +1,24 @@
+<?php 
+require_once "./lib/functions.php";
+
+$portal = array(
+                "自然科学与工程技术" => "http://epub.cnki.net/kns/oldnavi/n_Navi.aspx?NaviID=116&Flg=", 
+                //"人文社会科学"       => "http://epub.cnki.net/kns/oldnavi/n_Navi.aspx?NaviID=117&Flg="
+			   );
+
+foreach($portal as $k=>$u)
+{
+	$content = file_get($u);
+	$urls = parse_entry_page($content);
+	foreach($urls as $c=>$pageu)
+	{
+		echo "Begin to get $c\n";
+		$info = get_a_class_of_qikan($pageu);
+		//echo "SAVE $c\n";
+		//TODO 保存
+		echo "End get $c\n";
+	}
+}
+
+
+?>
