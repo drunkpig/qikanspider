26cf0a48ca2dd2f65a660849381e3e6a3f167bfb
Jon <xuchaoo@gmail.com>
2015-08-30 00:33:08 +0800

    update

--- install_data/_install.py
+++ install_data/_install.py
@@ -10,7 +10,7 @@ file_list = {
     "11185" : "../11185/11185_detail.log",
     "cnki" : "../cnki/cnki_detail.log",
     "cqvip" : "../cqvip/cqvip_detail.log",
-    "wanfang" : "../wanfang/wanfang_detail.log",
+    "wanfang" : "../wanfang/wf_detail.log",
     "emuch_zh" : "../emuch/emuch_detail_zh.log",
     "emuch_en" : "../emuch/emuch_detail_en.log"
     }
