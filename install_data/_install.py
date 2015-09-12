22ee2f297e098acbb1a65cd912bcf57a7129dadd
Jon <xuchaoo@gmail.com>
2015-09-12 21:04:26 +0800

    fix bug

--- install_data/_install.py
+++ install_data/_install.py
@@ -218,7 +218,7 @@ with fileinput.input("./temp") as final_result_file:
         #  图片编码为base64
         #  TODO
         imageFile = string.get('feng_mian')
-        if imageFile and os.path.exists(imageFile):
+        if len(imageFile)>0 and os.path.exists(imageFile):
             b64Img = image_b64_encode(imageFile)
             string['feng_mian'] = b64Img
         else:
