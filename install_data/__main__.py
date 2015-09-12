a459fe8a9334cc16e327bbc002ea95895da3c55f
Jon <xuchaoo@gmail.com>
2015-09-12 22:29:08 +0800

    fix bug

--- install_data/__main__.py
+++ install_data/__main__.py
@@ -2,9 +2,10 @@ import base64
 
 __author__ = 'cxu'
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-db = MongoClient().db_test
-collection = db.image_test
+db = MongoClient(host="120.26.0.133").db_qikan
+collection = db.qikan_info
 
 def insert_image(imageFile):
     with open(imageFile, "rb") as image_file:
@@ -20,15 +21,16 @@ def retrieve_image(obj_id):
     #  data_json = json.loads(data)
     #  img1 = data_json['image']
     #  decode=img1.decode()
-    imgB64Str = data['image']
+    imgB64Str = data['feng_mian']
     imgB64Str = imgB64Str.decode()
     img_tag = '<img alt="sample" src="data:image/png;base64,{0}">'.format(imgB64Str)
     print(img_tag)
     return imgB64Str
 
-imgFile = "../cnki/img/0fbf6defca7f490792b83eeb9abf19ae.jpg"
-print("insert")
-obj_id = insert_image(imgFile)
-print("inserted id %s" % obj_id, end="\n")
+# imgFile = "../cnki/img/0fbf6defca7f490792b83eeb9abf19ae.jpg"
+# print("insert")
+# obj_id = insert_image(imgFile)
+# print("inserted id %s" % obj_id, end="\n")
+obj_id = ObjectId("55f43212c777807b3db5024c");
 string = retrieve_image(obj_id)
 print(string)
